236be2ca53771966deccc24fff624356354d19ec
OussemaHdr <51028922+OussemaHdr@users.noreply.github.com>
2019-05-25 20:02:32 +0200

    Update Image_Orginazer.py

--- utils/Image_Orginazer.py
+++ utils/Image_Orginazer.py
@@ -2,14 +2,14 @@ import pandas as pd
 import os
 from tqdm import tqdm
 from PIL import Image
-data = pd.read_csv('C:/Users/SBS/Desktop/GT-final_test.csv', sep=';')
+data = pd.read_csv('.../GT-final_test.csv', sep=';')
 
-pictures_folder = 'C:/Users/SBS/Desktop/Final_Test/Images'
+pictures_folder = '...'
 target = 'Test_full'
 
 #creating multiple folders inside the main folder (tsawer) 
 for i in range (0,43):
-        os.mkdir('C:/Users/SBS/Desktop/Test_full/' + str(i))
+        os.mkdir('.../Test_full/' + str(i))
 
 #puting every picture in its associated class folder
 for item in tqdm(range(0, data.shape[0])):
